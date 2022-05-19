9dd48ed7c44c0f59bee8b2283e2368dc6d5bd768
jfremond <jfremond@student.42.fr>
2022-05-19 16:34:45 +0200

    ex00 done

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,16 +6,16 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 15:45:44 by jfremond          #+#    #+#             */
-/*   Updated: 2022/05/19 16:25:00 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/05/19 16:34:24 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void upper(std::string str)
+void convert_to_uppercase(std::string str)
 {
-	for (std::basic_string<char>::size_type j = 0; j < str.length(); j++)
-			str[j] = toupper(str[j]);
+	for (std::string::size_type i = 0; i < str.length(); i++)
+			str[i] = toupper(str[i]);
 		std::cout << str;
 }
 
@@ -24,13 +24,13 @@ int	main(int argc, char **argv)
 	std::string no_arg = "* loud and unbereable feedback noise *";
 	if (argc == 1)
 	{
-		upper(no_arg);
+		convert_to_uppercase(no_arg);
 		std::cout << std::endl;
 	}
 	else
 	{
-		for (int i = 1; i < argc; i++)
-			upper(argv[i]);
+		for (int j = 1; j < argc; j++)
+			convert_to_uppercase(argv[j]);
 		std::cout << std::endl;
 	}
 	return (0);
