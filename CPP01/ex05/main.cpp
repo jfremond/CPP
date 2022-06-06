cd08440d2fd4716556c719e51c4c324a06223e21
jfremond <jfremond@student.42.fr>
2022-06-06 11:39:49 +0200

    Finished ex05

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 11:16:24 by jfremond          #+#    #+#             */
-/*   Updated: 2022/06/06 11:27:55 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/06/06 11:39:33 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int	main(int argc, char **argv)
 {
+	Harl	harl;
+	
 	if (argc != 2)
 	{
 		std::cout << "Usage: ./Harl2.0 [level]" << std::endl;
 		return (1);
 	}
-	Harl	harl;
 	harl.complain(argv[1]);
 	return (0);
 }
