b40e1fa13c5f59cdd8930f05f9a7095026b5546e
jfremond <jfremond@student.42.fr>
2022-06-10 17:13:01 +0200

    ex03 done

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 15:21:14 by jfremond          #+#    #+#             */
-/*   Updated: 2022/06/09 23:14:07 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/06/10 14:54:51 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,37 @@ const int	Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed(const int val) : _value(val << this->_bits)
 {
-	std::cout << "Int constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed(const float val) : _value(roundf(val * (1 << this->_bits)))
 {
-	std::cout << "Float constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed(Fixed const &src)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 	return ;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
 	return ;
 }
 
 int	Fixed::getRawBits(void) const
 {
-	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->_value);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	// std::cout << "setRawBits member function called" << std::endl;
 	this->_value = raw;
 	return ;
 }
@@ -70,7 +63,6 @@ int	Fixed::toInt(void) const
 
 Fixed &Fixed::operator=(Fixed const &rhs)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &rhs)
 		this->_value = rhs.getRawBits();
 	return (*this);
--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,28 +6,67 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 15:19:31 by jfremond          #+#    #+#             */
-/*   Updated: 2022/06/09 20:11:13 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/06/10 17:12:21 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int	main(void)
-{
-	Fixed 		a;
-	Fixed const	b(Fixed(5.05f) * Fixed(2));
+{	
+	Fixed	a(12.9f);
+	Fixed	b(10.4f);
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "\033[33mFirst values of a and b :\033[0m" << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of b : " << b << std::endl << std::endl;
 
-	std::cout << b << std::endl;
+	std::cout << "\033[33mTest comparisons : "
+		<<  "a > b, a < b, a >= b, a <= b, a == b, a != b\033[0m" << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of a > b : " << (a > b) << std::endl;
+	std::cout << "Value of a < b : " << (a < b) << std::endl;
+	std::cout << "Value of a >= b : " << (a >= b) << std::endl;
+	std::cout << "Value of a <= b : " << (a <= b) << std::endl;
+	std::cout << "Value of a == b : " << (a == b) << std::endl;
+	std::cout << "Value of a != b : " << (a != b) << std::endl << std::endl;
 	
-	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << "\033[33mTest arithmetic operations : "
+		<< "a + 56, a - 45.2f, b * 3.4f, b / 6\033[0m" << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of a + 56 : " << a + 56 << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of a - 45.2f : " << a - 45.2f << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of b * 3.4f : " << b * 3.4f << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of b / 6 : " << b / 6 << std::endl;
+	std::cout << "Value of b : " << b << std::endl << std::endl;
+	
+	std::cout << "\033[33mTest incrementation : "
+		<< "++a, a++\033[0m" << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of ++a : " << ++a << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of a++ : " << a++ << std::endl;
+	std::cout << "Value of a : " << a << std::endl << std::endl;
 
-	std::cout << Fixed::min(a, b) << std::endl;
+	std::cout << "\033[33mTest decrementation : "
+		<< "--b, b--\033[0m" << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of --b : " << --b << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of b-- : " << b-- << std::endl;
+	std::cout << "Value of b : " << b << std::endl << std::endl;
+
+	std::cout << "\033[33mTest min and max : "
+		<< "min(a, b), max(a, b)\033[0m" << std::endl;
+	std::cout << "Value of a : " << a << std::endl;
+	std::cout << "Value of b : " << b << std::endl;
+	std::cout << "Value of min(a, b) : " << Fixed::min(a, b) << std::endl;
+	std::cout << "Value of max(a, b) : " << Fixed::max(a, b) << std::endl << std::endl;
 	
 	return (0);
 }
