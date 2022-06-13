eeba8e68aace89d888bbd3dc04db4155201a1d7e
jfremond <jfremond@student.42.fr>
2022-06-13 15:47:46 +0200

    ex00 done

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,13 +6,13 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 12:26:38 by jfremond          #+#    #+#             */
-/*   Updated: 2022/06/13 15:34:58 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/06/13 15:47:37 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(void) : _name("clap-clap"), _hit_pts(10), _en_pts(10), _atk_dmg(0)
+ClapTrap::ClapTrap(void) : _name("Default"), _hit_pts(10), _en_pts(10), _atk_dmg(0)
 {
 	std::cout << "\033[34mClapTrap " << this->_name << " \033[0mconstructor called" << std::endl;
 	return ;
