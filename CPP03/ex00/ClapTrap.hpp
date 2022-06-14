6c3c751eb7c795bd07994910356eb022d1da84d5
jfremond <jfremond@student.42.fr>
2022-06-14 10:09:58 +0200

    ex00 done

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 12:26:26 by jfremond          #+#    #+#             */
-/*   Updated: 2022/06/13 15:46:47 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/06/14 10:09:46 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 class ClapTrap
 {
 	private:
-		std::string		_name;
-		int	_hit_pts;
-		int	_en_pts;
-		int	_atk_dmg;
+		std::string	_name;
+		int			_hit_pts;
+		int			_en_pts;
+		int			_atk_dmg;
 	public:
 		ClapTrap(void);
 		ClapTrap(std::string name);
