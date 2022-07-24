4d5ef91d5d8da5c1c84d25481c13f968935663bf
jfremond <jfremond@student.42.fr>
2022-07-24 15:04:55 +0200

    ex02 done

---
+++ CPP03/ex02/FragTrap.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 14:50:29 by jfremond          #+#    #+#             */
+/*   Updated: 2022/07/24 15:01:18 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void) : ClapTrap()
+{
+	std::cout << GREEN << "FragTrap " << this->_name << RESET << " default constructor called" << std::endl;
+	return ;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << GREEN << "FragTrap " << this->_name << RESET << " name constructor called" << std::endl;
+	return ;
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << GREEN << "FragTrap " << this->_name << RESET << " destructor called" << std::endl;
+	return ;
+}
+
+FragTrap::FragTrap(FragTrap const &src) : ClapTrap(src)
+{
+	*this = src;
+	std::cout << GREEN << "FragTrap " << this->_name << RESET << " copy constructor called" << std::endl;
+	return ;
+}
+
+FragTrap &FragTrap::operator=(FragTrap const &rhs)
+{
+	this->_name = rhs._name;
+	this->_hit_pts = rhs._hit_pts;
+	this->_en_pts = rhs._en_pts;
+	this->_atk_dmg = rhs._atk_dmg;
+	return (*this);
+}
+
+void	FragTrap::highFivesGuys(void)
+{
+	std::cout << GREEN << "FragTrap " << this->_name << RESET << " does a high five!" << std::endl;	
+}
---
+++ CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 14:43:52 by jfremond          #+#    #+#             */
+/*   Updated: 2022/07/24 14:54:09 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	private:
+	public:
+		FragTrap(void);
+		FragTrap(std::string name);
+		FragTrap(FragTrap const &src);
+		FragTrap &operator=(FragTrap const &rhs);
+		~FragTrap(void);
+		void	highFivesGuys(void);
+};
+
+#endif
