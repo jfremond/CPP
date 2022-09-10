bdf429799c68bad071e5fcfa4cc66c5053ea325e
jfremond <jfremond@student.42.fr>
2022-09-10 04:29:39 +0200

    Adding ex03 - Things to test :     - Equip too many Materia     - use function with a number not between 0 and 4     - equip something that is not ice or cure

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 05:41:33 by jfremond          #+#    #+#             */
-/*   Updated: 2022/09/09 06:25:36 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/09/10 04:25:08 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,29 @@ int main()
 	ICharacter* me = new Character("me");
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
+	AMateria* tmp2;
+	tmp2 = src->createMateria("ice");
+	AMateria* tmp3;
+	tmp3 = src->createMateria("ice");
 	me->equip(tmp);
+	me->equip(tmp2);
+	me->equip(tmp3);
+	// me->equip(tmp);
 	AMateria *tmp1;
 	tmp1 = src->createMateria("cure");
 	me->equip(tmp1);
 
-	ICharacter* bob = new Character("bob");
-	me->use(0, *bob);
+	ICharacter* bob = new Character("Bob");
+	me->use(4, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
 
 	delete bob;
-	delete tmp1;
-	delete tmp;
+	// ! Pas de delete les tmp parce que c'est fait dans le destructor de Character
+	// ! T'as quand même segfault longtemps à cause de ça
+	// delete tmp1;
+	// delete tmp;
 	delete me;
 	delete src;
     return (0);
