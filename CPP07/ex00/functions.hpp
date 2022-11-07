8a610cbdc4813fb964bdf8dcfd8e80f6508a0d11
jfremond <jfremond@student.42.fr>
2022-11-07 21:52:11 +0100

    Adding CPP/ex00 - Need to do beautiful main

---
+++ CPP07/ex00/functions.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 20:39:32 by jfremond          #+#    #+#             */
+/*   Updated: 2022/11/07 21:46:19 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include <iostream>
+
+template<typename T>
+void	swap(T &a, T &b)
+{
+	T tmp;
+	
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T const	&min(T const &a, T const &b)
+{
+	return (a < b ? a : b);
+}
+
+template<typename T>
+T const	&max(T const &a, T const &b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
