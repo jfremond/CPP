da2580479667d81e8fc5ce30fae224eae4040db3
jfremond <jfremond@student.42.fr>
2023-03-23 04:01:34 +0100

    Adding school version

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 00:28:30 by jfremond          #+#    #+#             */
-/*   Updated: 2023/03/21 17:37:46 by jfremond         ###   ########.fr       */
+/*   Updated: 2023/03/23 03:58:07 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,30 @@
 class BitcoinExchange
 {
 	private:
-		std::map<std::string, double>	_data;
-		//! Rename bool because it covers all errors
-		bool							_stream_error;
-		//& Convert a string to a double
-		double							_stod(std::string str_to_cast);
-		//& Convert a double to a string
-		std::string						_dtos(double val_to_cast);
-		void							_fillData();
-		void							_printRes();
 		BitcoinExchange();
+		std::map<std::string, double>					_data;
+		std::map<std::string, double>::const_iterator	it;
+		int												_error;
+		double											_stod(std::string str_to_cast);
+		std::string										_dtos(double val_to_cast);
+		void											_fillData();
+		void											_printRes(std::string const filename);
+		int												_checkDateValidity(std::string str);
 	public:
 		BitcoinExchange(std::string const filename);
 		BitcoinExchange(BitcoinExchange const &src);
 		BitcoinExchange	&operator=(BitcoinExchange const &rhs);
 		~BitcoinExchange();
 		void	printData();
-
 };
 
 //--------------- PRIVATE FUNCTIONS ----------------//
+
+BitcoinExchange::BitcoinExchange() : _error(0)
+{
+	_fillData();
+	std::cout << "Default constructor" << std::endl;
+}
 
 double	BitcoinExchange::_stod(std::string str_to_cast)
 {
@@ -52,8 +56,9 @@ double	BitcoinExchange::_stod(std::string str_to_cast)
 	
 	if (stream.fail())
 	{
-		std::cout << str_to_cast << std::endl;
-		std::cout << "Error: stringstream has failed" << std::endl;
+		_error = 1;
+		// std::cout << str_to_cast << std::endl;
+		// std::cout << "Error: stringstream has failed" << std::endl;
 	}
 	return (res);
 }
@@ -70,8 +75,8 @@ std::string	BitcoinExchange::_dtos(double val_to_cast)
 	
 	if (stream.fail())
 	{
-		_stream_error = true;
-		std::cout << "Error: stringstream has failed" << std::endl;
+		_error = 1;
+		// std::cout << "Error: stringstream has failed" << std::endl;
 	}
 	return (res);
 }
@@ -95,11 +100,11 @@ void	BitcoinExchange::_fillData()
 	}
 }
 
-void	BitcoinExchange::_printRes()
+void	BitcoinExchange::_printRes(std::string const filename)
 {
 	std::ifstream	file;
 	std::string		line;
-	file.open("input.txt");
+	file.open(filename.c_str());
 	if (!file)
 	{
 		std::cout << "Error: cannot open file" << std::endl;
@@ -118,44 +123,126 @@ void	BitcoinExchange::_printRes()
 	std::string s3 = line.substr(0, line.length());
 	if (s3 != "value")
 		std::cout << "Error: invalid header value" << std::endl;
-	std::cout << "Header is valid" << std::endl;
+	int i = 1;
 	while (!file.eof())
 	{
 		getline(file, line);
 		if (!file.eof())
 		{	
 			std::string s1 = line.substr(0, 10);
-			//& Function to check date validity here
-			std::cout << "[" << s1 << "]" << "\t";
+			_checkDateValidity(s1);
+			// std::cout << _error << "\t";
+			// std::cout << s1 << "\t";
+			try
+			{
+				it = _data.upper_bound(s1);
+				it--;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+			
 			line.erase(0, s1.length());
 			std::string	s2 = line.substr(0, 3);
-			//& Function to check separator validity here
-			std::cout << "[" << s2 << "]" << "\t";
+			if (_error == 0 && s2 != " | ")
+				_error = 3;
 			line.erase(0, s2.length());
 			std::string s3 = line.substr(0, line.length());
-			//& Function to check value validity here
-			std::cout << "[" << s3 << "]" << std::endl;	
+			// std::cout << s3 << "\t";
+			// std::cout << _error << "\t";
+			if (_error == 0 && _stod(s3) < 0)
+				_error = 4;
+			if (_error == 0 && _stod(s3) > 1000)
+				_error = 5;
+			// std::cout << _error << "\t";
+			if (_error == 2)
+				std::cout << "Error: bad input => " << s1 << std::endl;
+			if (_error == 3)
+				std::cout << "Error: delimitor is invalid" << std::endl;
+			if (_error == 4)
+				std::cout << "Error: not a positive number." << std::endl;
+			if (_error == 5)
+				std::cout << "Error: too large a number." << std::endl;
+			else if (_error == 0)
+				std::cout << s1 << " => " << s3 << " = " << _stod(s3) * it->second << std::endl;
 		}
+		++i;
+		_error = 0;
 	}
 }
 
-BitcoinExchange::BitcoinExchange() : _stream_error(false)
+int	BitcoinExchange::_checkDateValidity(std::string str)
 {
-	_fillData();
-	std::cout << "Default constructor" << std::endl;
+	std::string	tmp = str;
+	std::string	y;
+	std::string	m;
+	std::string d;
+	// std::cout << str[4] << " " << str[7] << " " << std::endl;
+	if (tmp.length() != 10)
+	{
+		_error = 2;
+		// std::cout << "wrong length" << std::endl;
+		return (1);
+	}
+	else if (str[4] != '-' || str[7] != '-')
+	{
+		_error = 2;
+		// std::cout << "wrong delim" << std::endl;
+		return (1);
+	}
+	y = tmp.substr(0, 4);
+	if (y < "2009" || y > "2022")
+	{
+		// std::cout << y << std::endl;
+		_error = 2;
+		return (1);
+		// std::cout << y << std::endl;
+		// std::cout << "wrong date" << std::endl;
+	}
+	tmp.erase(0, y.length());
+	tmp.erase(0, 1);
+	m = tmp.substr(0, 2);
+	if (m < "01" || m > "12")
+	{
+		// std::cout << m << std::endl;
+		_error = 2;
+		return (1);
+		// std::cout << m << std::endl;
+		// std::cout << "wrong month" << std::endl;
+	}
+	tmp.erase(0, m.length());
+	tmp.erase(0, 1);
+	d = tmp.substr(0, 2);
+	if (d < "01" || d > "31")
+	{
+		_error = 2;
+		return (1);
+		// std::cout << d << std::endl;
+		// std::cout << "wrong day" << std::endl;
+	}
+	if (str < "2009-01-02")
+	{
+		_error = 2;
+		return (1);
+		// std::cout << "tmp: " << str << std::endl;
+		// std::cout << "Date too early" << std::endl;
+	}
+	// std::cout << y << " " << m << " " << d << " " << str << std::endl;
+	return (0);
 }
+
 
 //--------------- PUBLIC FUNCTIONS ----------------//
 
-BitcoinExchange::BitcoinExchange(std::string const filename) : _stream_error(false)
+BitcoinExchange::BitcoinExchange(std::string const filename) : _error(0)
 {
-	(void)filename;
 	_fillData();
-	_printRes();
 	std::cout << "String constructor" << std::endl;
+	_printRes(filename);
 }
 
-BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) : _data(src._data), _stream_error(src._stream_error)
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) : _data(src._data), _error(src._error)
 {
 	std::cout << "Copy constructor" << std::endl;
 }
@@ -163,7 +250,7 @@ BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) : _data(src._data),
 BitcoinExchange	&BitcoinExchange::operator=(BitcoinExchange const &rhs)
 {
 	_data = rhs._data;
-	_stream_error = rhs._stream_error;
+	_error = rhs._error;
 	std::cout << "Assignment operator overload" << std::endl;
 	return (*this);
 }
--- CPP09/ex00/TODO.md
+++ CPP09/ex00/TODO.md
@@ -1,7 +1,18 @@
 TODO:
 
 - [x] Check the number of arguments
-- [] Check the first line of input.txt is "date | value"
-- [] Check date is valid on each line (Year-Month-Day)
-- [] Check value is valid on each line (0 <= x <= 1000)
-- [] Implement a function that picks the previous date if the date in input.txt is not in data.csv (check month, month, then day)
+- [x] Check the first line of input.txt is "date | value"
+- [x] Check date is valid on each line (Year-Month-Day)
+- [x] Check value is valid on each line (0 <= x <= 1000)
+- [x] Implement a function that picks the previous date if the date in input.txt is not in data.csv (check year, month, then day)
+- [x] Assign different error codes depending on the errors you can find in the exercise
+	- _stod/_dtos conversion has failed				1
+	- date format is invalid						2
+	- delimiter is invalid " | " <- I mean that		3
+	- value is invalid (value < 0)					4
+	- value is invalid (value > 1000)				5
+
+- [x] solve segfault when using upper_bound
+
+- [ ] Create .cpp file and move functions there
+- [ ] Clean your mess
